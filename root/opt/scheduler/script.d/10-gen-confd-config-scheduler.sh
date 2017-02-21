0a4ef857e53a6f7b3da83cde32c1d46469ee8da9
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-21 09:49:25 +0100

    Fix templates

--- root/opt/scheduler/script.d/10-gen-confd-config-scheduler.sh
+++ root/opt/scheduler/script.d/10-gen-confd-config-scheduler.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/with-contenv sh
 
-exec cat << EOF > ${SCHEDULER_VOLUME}/confd/etc/conf.d/scheduler.cfg.toml
+cat << EOF > ${SCHEDULER_VOLUME}/confd/etc/conf.d/scheduler.cfg.toml
 [template]
-prefix = "/2015-12-19/self"
+prefix = "/2015-12-19"
 src = "scheduler.cfg.tmpl"
 dest = "${SCHEDULER_VOLUME}/conf/scheduler.cfg"
 mode = "0644"
 keys = [
-  "service",
-  "container",
-  "stack",
-  "host"
+  "/self/service",
+  "/self/container",
+  "/self/stack",
+  "/self/host",
+  "/containers",
 ]
 EOF
--- root/opt/scheduler/script.d/11-init-confd.sh
+++ root/opt/scheduler/script.d/11-init-confd.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/with-contenv sh
 
-
-exec ${SCHEDULER_VOLUME}/bin/confd -confdir ${SCHEDULER_VOLUME}/confd/etc -sync-only -onetime -backend rancher
+${SCHEDULER_VOLUME}/confd/bin/confd -confdir ${SCHEDULER_VOLUME}/confd/etc -sync-only -onetime -backend rancher
